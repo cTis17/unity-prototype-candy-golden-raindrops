4dd0744bb6fca9c6208e0ab718e9d46ec023a6d2
cTis17 <christisdale17@gmail.com>
2019-06-15 16:46:06 -0500

    Initial commit

---
+++ catcher/Assets/BoundsBehavior.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BoundsBehavior : MonoBehaviour
+{
+    
+    public int score;
+    private List<GameObject> basketList;
+    public GameObject basketPrefab;
+    private AudioSource[] audioSources;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSources = GetComponents<AudioSource>();
+        basketList = new List<GameObject>();
+        float bottomEdge = transform.position.y + .5f;
+        for (int i = 3; i >0; i--)
+        {
+            GameObject basket = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = basket.transform.position;
+            pos.y = bottomEdge + (i * 0.5f);
+            basket.transform.position = pos;
+            basketList.Add(basket);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "spawn")
+        {
+            audioSources[1].Play();
+            int bottomBasket = basketList.Count -1;
+            if (bottomBasket > -1)
+            {
+                Destroy(basketList [bottomBasket]);
+                basketList.RemoveAt(bottomBasket);
+            }
+            GameObject[] spawnArray = GameObject.FindGameObjectsWithTag("spawn");
+            foreach (GameObject spawn in spawnArray)
+            {
+                Destroy(spawn);
+            }
+
+            if (bottomBasket < 1)
+            {
+                Destroy(GameObject.Find("Coin"));
+                Invoke("RestartScene", 3.0f);
+            }
+        }
+    }
+
+    private void RestartScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+
+    public void IncrementScore()
+    {
+        score++;
+        audioSources[0].Play();
+        GameObject.Find("ScoreOutput").GetComponent<Text>().text = score.ToString();
+    }
+
+}
---
+++ catcher/Assets/coin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    public float coinSpeed = 2.0f;
+    public GameObject spawnPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Invoke("DropSpawn", 1.0f); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(coinSpeed * Time.deltaTime, 0, 0);
+        if (Mathf.Abs(transform.position.x) > 7.0f) {
+            coinSpeed *= -1.0f;
+        }
+    }
+    
+    void DropSpawn()
+    {
+        GameObject spawn = Instantiate<GameObject>(spawnPrefab);
+        Invoke("DropSpawn", 0.5f);
+        spawn.transform.position = transform.position;
+    }
+    // private void OnCollisionEnter2d(Collision2D collision){
+    //     if (collision.gameObject.name == "Bounds"){
+    //         coinSpeed*= -1.0f;
+    //     }
+    // }
+}
